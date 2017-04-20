8f0bfc27c85d1266a660e9717ac5dbdb30dbd4c0
Juan Broz <f870421@gmail.com>
2017-04-20 15:26:32 -0500

    Quick fix to disable authentication on parking_info

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,6 +1,6 @@
 class ParkingsController < ApplicationController
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:parking_info]
   
   def index
     @parkings = Parking.all
